d1193344fe94ab6b2572a3ca75fac65cc0c1e38b
EmmanuelEskander07 <34535442+EmmanuelEskander07@users.noreply.github.com>
2019-02-15 09:04:48 +0100

    Add files via upload

--- TpNode2/controllers/client.controller.js
+++ TpNode2/controllers/client.controller.js
@@ -1,12 +1,15 @@
 const Client = require('../models/client.model.js');
 
-exports.createProduct = function (req,res) {
+exports.createClient = function (req,res) {
     let client = new Client(
         {
             nom: req.body.nom,
             address: req.body.address,
             cp: req.body.cp,
             ville: req.body.ville,
+            refnom: req.body.refnom,
+            refprenom: req.body.refprenom,
+            refposte: req.body.refposte,
             telephone: req.body.telephone,
             mail: req.body.mail,
             prospet: req.body.prospet
